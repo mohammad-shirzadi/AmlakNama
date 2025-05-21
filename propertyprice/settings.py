0178cb319b3d0e46edc46a499402d9928f48dcde
mohammad <m.shirzady44@gmail.com>
2025-05-21 11:51:39 +0330

    add secret key from env

--- propertyprice/settings.py
+++ propertyprice/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--(n7s=hh0p48&)+z$tqxdxt*3+p^+^9r)r2h=^f4f!l6$1j7xe'
+#SECRET_KEY = 'django-insecure--(n7s=hh0p48&)+z$tqxdxt*3+p^+^9r)r2h=^f4f!l6$1j7xe'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
