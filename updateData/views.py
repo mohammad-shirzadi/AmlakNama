b2d1ce0ff5021d0b24f90c595c2befd3b84ace7e
mohammad <m.shirzady44@gmail.com>
2025-07-05 22:19:33 +0330

    add stop func and clearing

--- updateData/views.py
+++ updateData/views.py
@@ -2,46 +2,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.admin.views.decorators import staff_member_required
-from .services import log, update,cdt
+from .services import log, logreader, update, cdt, stop_update, STOPSIGN
+
 
 
 @staff_member_required
 def updatePg(request):
-    #TODO create variable that show the upadate is running and stope whene html closed(?!)
+    #TODO create variable that show the upadate is running and stope whene html closed(?!) 
+    global STOPSIGN
 
-
-    def logreader():
-        with open("log",'r') as file:
-            logtxt = file.readlines()
-            if not logtxt: 
-                logtxt = ['']
-            return logtxt[-1]
-
-
-    context = {
-        "count_res_rent" : cdt('res','rent')[0],
-        "lastupdateRR" : cdt('res','rent')[1],
-        "count_res_buy" : cdt('res','buy')[0],
-        "lastupdateRB" : cdt('res','buy')[1],
-        "count_resland_buy" : cdt('resland','buy')[0],
-        "lastupdateRlB" : cdt('resland','buy')[1],
-        "count_com_buy" : cdt('com','buy')[0],
-        "lastupdateCB" : cdt('com','buy')[1],
-        "count_com_rent" : cdt('com','rent')[0],
-        "lastupdateCR" : cdt('com','rent')[1],
-        "inlog" : logreader()
-    }
-
-    if request.method == "POST" and request.POST.get('a'):
-        return JsonResponse({'log': logreader()})
-    
     if request.method == 'POST':
+        log(request.POST)
+
+        STOPSIGN = False
+
+        if request.POST.get('LogKey'):
+            return JsonResponse({'log': logreader()})
+        
         if request.POST.get('res-rent'):
             try:
                 update('res','rent')
                 context['log'] = "update('res','rent') is done"
             except Exception as erore:
                 log(str(erore)+'-')
+        
         if request.POST.get('res-buy'):
             try:
                 update('res','buy')
@@ -70,7 +54,27 @@ def updatePg(request):
             except Exception as erore:
                 log(str(erore)+'-')
     elif request.method == 'GET':
+        STOPSIGN = False
         pass
+
+    context = {
+        "count_res_rent" : cdt('res','rent')[0],
+        "lastupdateRR" : cdt('res','rent')[1],
+        "count_res_buy" : cdt('res','buy')[0],
+        "lastupdateRB" : cdt('res','buy')[1],
+        "count_resland_buy" : cdt('resland','buy')[0],
+        "lastupdateRlB" : cdt('resland','buy')[1],
+        "count_com_buy" : cdt('com','buy')[0],
+        "lastupdateCB" : cdt('com','buy')[1],
+        "count_com_rent" : cdt('com','rent')[0],
+        "lastupdateCR" : cdt('com','rent')[1],
+        "inlog" : logreader()
+    }
     return render(request,'admin/updatePg.html', context)
 
 
+def stop_updatePg(request):
+    print(request.POST)
+    if request.POST.get('stop'):
+        stop_update()
+        return render(request,'admin/updatePg.html')
