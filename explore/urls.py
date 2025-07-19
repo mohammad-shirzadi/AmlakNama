fd35cedb4212465a0b483c680b88742c43f33504
mohammad <m.shirzady44@gmail.com>
2025-07-19 13:10:27 +0330

    fix bug

--- explore/urls.py
+++ explore/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path("",views.homeAPI.as_view(), name='home'),
     path("index",views.homeAPI.as_view(), name='home'),
-    path("explore", views.explore, name='explore'),
-    path("contactus", views.contactus, name='contactus'),
+    path("explore", views.exploreAPI.as_view(), name='explore'),
+    path("contactus", views.contactusAPI.as_view(), name='contactus'),
 ]
